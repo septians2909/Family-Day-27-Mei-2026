b18017222bae1daa2f3b51d0a5d1dd75aa03e410
septians2909 <septians2909@gmail.com>
2026-03-16 11:51:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const gifStages = [
 const noMessages = [
     "No",
     "Are you positive? 🤔",
-    "Moni please... 🥺",
+    "Pleasee... 🥺",
     "If you say no, I will be really sad...",
     "I will be very sad... 😢",
     "Please??? 💔",
